73e9e6bb3ec4ea10b818d1405c88159bae81bba6
Nathan Harwit <nharwit@gmail.com>
2023-05-28 23:56:48 -0600

    WORKING perfectly for just this one test, changed gaussian blur to 9,9 kernel size from 5,5

--- detectLastMove.py
+++ detectLastMove.py
@@ -5,13 +5,13 @@ def detect_circles(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Apply Gaussian blur to preprocess image better, it actually does make a difference
-    gray_blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    gray_blur = cv2.GaussianBlur(gray, (9, 9), 0)
 
     # Parameters for Hough Circle Transform
-    dp = 1          # default value
-    minDist = 20    # default value
-    param1 = 50     # default value
-    param2 = 30     # default value
+    dp = 1          # default value 1
+    minDist = 1    # default value 20
+    param1 = 50     # default value 50
+    param2 = 30    # default value 30
     minRadius = 10  # between 10 and 15 radius gets you the hollow inner circle
     maxRadius = 15
 
